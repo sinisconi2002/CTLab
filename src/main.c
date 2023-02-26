91ba9af257511e591c8616e4e838307c108d05ce
Simion-Gheorghe-Leontino Inișconi <simioninisconi@gmail.com>
2023-02-26 02:25:08 +0200

    refactor the place of source files

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "messages.c"
 
 int main() {
     printf("Hello, World!\n");
+    //printMessage();
     return 0;
 }
---
+++ src/messages.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void printMessage(){
+    printf("Let's see what the hack am I doing in this project:)");
+}
